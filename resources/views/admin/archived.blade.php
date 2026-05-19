9a16e46e52c31ba08a6dc2473d147ca7f1b7aa1a
kirudevv <ruimochii@gmail.com>
2026-05-19 16:51:35 +0800

    i can feel it

--- resources/views/admin/archived.blade.php
+++ resources/views/admin/archived.blade.php
@@ -4,5 +4,7 @@
     <h1 class="font-semibold text-3xl text-center py-5 text-[#3A384C]">Archives</h1>
     <a href="{{ route('dashboard') }}" class="bg-[#A93232] text-end py-2 px-4 text-[#feffea] rounded-xl mb-2">back</a>
     </div>
-    @include('tables.donationarchives')
+    <div class="bg-[#DE6262] w-300 flex flex-col max-w-full overflow-x-auto px-3 justify-center rounded-4xl shadow-md mt-3 mb-6 md:w-350">
+        <div class="py-4" >@include('tables.donationarchives')</div>
+    </div>
 @endsection
